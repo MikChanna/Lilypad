28c44e3cfa9f66c8be1c99f97dd8d692ba95697e
Sarah Kerr <sarah.kerr93@gmail.com>
2020-10-19 21:25:04 -0400

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Browse from "./pages/Browse";
+import Chat from "./pages/Chat";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/browse">
+              <Browse />
+            </Route>
+            <Route exact path="/chat">
+              <Chat />
+            </Route>
+            <Route>
+              <Page404 />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
