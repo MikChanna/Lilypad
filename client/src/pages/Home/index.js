802fe3fff34ddc4d528ab3f8f40df74445c0bc90
Sarah Kerr <sarah.kerr93@gmail.com>
2020-10-21 21:15:20 -0400

    pushing to main

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+// import families from "../../data/Family.json";
+// import children from "../../data/Child.json";
 
 function Home() {
-  return <h1>Home page! </h1>;
+  //   state = {
+  //     families,
+  //   };
+
+  return (
+    <div>
+      <h1>Home page! </h1>
+    </div>
+  );
 }
 
 export default Home;
