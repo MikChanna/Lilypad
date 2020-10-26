e0bdabf830525083b80328247a678545c655867e
MikChanna <channa.mik@gmail.com>
2020-10-25 21:40:03 -0400

    added body parser and passport initialization and session

--- server.js
+++ server.js
@@ -1,28 +1,46 @@
+// env PORT
+const PORT = process.env.PORT || 3001;
+
+// dependencies
 const express = require("express");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var path = require('path');
+
 const app = express();
 const routes = require("./routes");
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
-
-// static directory
-app.use(express.static(__dirname + "/client/public"));
-
-// add routes
-app.use(routes);
-
+// mongoose connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lilypad", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// static 
+app.use(express.static(path.join(__dirname, '/client/public')));
+
+// add routes
+app.use(routes);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// app is listening on PORT
   app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
