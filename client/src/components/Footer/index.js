9807fb5b012393c8478f7b43d96a4cc0cf799cb1
Sarah Kerr <sarah.kerr93@gmail.com>
2020-10-28 19:04:01 -0400

    fixed footer absolut positioning

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./style.css";
 
-function Footer(){
-
-    return <footer>
-        <div className="ui vertical footer segment">
-        <div className="ui container">
-        Icons made by <a href="https://www.flaticon.local/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.local/" title="Flaticon">www.flaticon.local</a>
-        </div>
-        </div>
-        </footer>
-
-  
-
+function Footer() {
+  return (
+    <div class="ui vertical footer segment">
+      <div class="ui container">
+        <hr />
+        Icons made by{" "}
+        <a href="https://www.flaticon.local/authors/freepik" title="Freepik">
+          Freepik
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.local/" title="Flaticon">
+          www.flaticon.local
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
