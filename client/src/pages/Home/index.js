1e83cfa9d75c388049e8c14bd1610ccb5df7be8c
MikChanna <channa.mik@gmail.com>
2020-10-28 21:43:32 -0400

    copied PulledData component into ChildData

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,23 +1,18 @@
 import React, { Component } from "react";
 import ChildData from "../../components/ChildData";
 import SeasonalVids from "../../components/SeasonalVids";
-import PulledChildData from "../../components/PulledChildData";
 
 //eventually replace with API call to specific family data of whoever is logged in
-import children from "../../data/Child.json";
-import families from "../../data/Family.json";
+// import children from "../../data/Child.json";
+// import families from "../../data/Family.json";
 
 class Home extends Component {
-  state = {
-    families,
-    children,
-  };
+ 
 
   render() {
     return (
       <div>
-        <ChildData children={children} />
-        <PulledChildData/>
+        <ChildData/>
         <SeasonalVids />
       </div>
     );
