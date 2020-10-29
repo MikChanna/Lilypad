8758f52cce5a30d84163b37e8ceaebebccd5ed56
MikChanna <channa.mik@gmail.com>
2020-10-28 21:39:31 -0400

    fixed api for loading childData

--- client/src/components/PulledChildData/index.js
+++ client/src/components/PulledChildData/index.js
@@ -1,58 +1,65 @@
 import React, {useState, useEffect} from "react";
-import API from "../../utils";
+import API from "../../utils/ChildAPI";
+var avatarDirectory = process.env.PUBLIC_URL + "/assets/images/boy_darkhair.png";
 
 function PulledData(){
 
-const [childData, setChildData] = useState;
+    const [childData, setChildData] = useState([]);
 
-// loads child information and strores them with setChildData
-useEffect(() => {
-    loadChildData()
-}, {})
+    // loads child information and strores them with setChildData
+    useEffect(() => {
+        loadChildData()
+      }, [])
 
-function loadChildData() {
-    API.getChild()
-    .then(res =>
-        setChildData(res.data)
-        )
-        .catch(err => console.log(err));
-}
+      useEffect(() => {
+        console.log(childData)
+      }, [childData])
 
-return  <div className="stackable ui grid container childData">
-    {this.childData.map((child) => (
-  <div className="five wide column" key={child.id}>
-    <center>
-      <h2 className="ui header">
-        <img
-          src={avatarDirectory + child.image}
-          className="ui circular image"
-          alt="avatar"
-        />
-        {child.firstName}
-      </h2>
+    function loadChildData() {
+        API.getChildren()
+        
+        .then(res =>
+            setChildData(res.data)
+            )
+            
+            .catch(err => console.log(err));
+    }
 
-      <div className="ui list">
-        <div className="item">
-          <div className="header">Allergies</div>
-          {child.allergies}
+    return  <div className="stackable ui grid container childData">
+        {childData.map(child => (
+    <div className="five wide column" key={child.id}>
+        <center>
+        <h2 className="ui header">
+            <img
+            src={avatarDirectory}
+            className="ui circular image"
+            alt="avatar"
+            />
+            {child.firstName}
+        </h2>
+
+        <div className="ui list">
+            <div className="item">
+            <div className="header">Allergies</div>
+            {child.allergies}
+            </div>
+            <div className="item">
+            <div className="header">Bedtime</div>
+            {child.bedtime}
+            </div>
+            <div className="item">
+            <div className="header">Favorite food</div>
+            {child.favoriteFood}
+            </div>
+            <div className="item">
+            <div className="header">Favorite activity</div>
+            {child.favoriteActivity}
+            </div>
         </div>
-        <div className="item">
-          <div className="header">Bedtime</div>
-          {child.bedtime}
-        </div>
-        <div className="item">
-          <div className="header">Favorite food</div>
-          {child.favoriteFood}
-        </div>
-        <div className="item">
-          <div className="header">Favorite activity</div>
-          {child.favoriteActivity}
-        </div>
-      </div>
-    </center>
-  </div>
-    ))}
-</div>
+        </center>
+    </div>
+        ))}
+    </div>
 
 }
 
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ChildData from "../../components/ChildData";
 import SeasonalVids from "../../components/SeasonalVids";
+import PulledChildData from "../../components/PulledChildData";
 
 //eventually replace with API call to specific family data of whoever is logged in
 import children from "../../data/Child.json";
@@ -16,6 +17,7 @@ class Home extends Component {
     return (
       <div>
         <ChildData children={children} />
+        <PulledChildData/>
         <SeasonalVids />
       </div>
     );
