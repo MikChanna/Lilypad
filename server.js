6f6d4c841f4abec9c0fbf475661da7ef9372e897
MikChanna <channa.mik@gmail.com>
2020-11-03 22:02:42 -0500

    edit middleware

--- server.js
+++ server.js
@@ -5,31 +5,47 @@ const PORT = process.env.PORT || 3001;
 const express = require("express");
 const mongoose = require("mongoose");
 var bodyParser = require('body-parser');
+const cors = require("cors");
 var passport = require('passport');
 var path = require('path');
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const app = express();
 const routes = require("./routes");
 
 // mongoose connection
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Lilypad',
+  process.env.MONGODB_URI || 'mongodb://localhost/copyLilypad',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  }
+  },
+  ()=> console.log("Connected to database")
 );
 
 // middleware
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(require('express-session')({
-    secret: 'keyboard cat',
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: __dirname, // <-- location of the react app were connecting to
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: 'secretcode',
     resave: false,
     saveUninitialized: false
 }));
+
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport")(passport);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -40,9 +56,6 @@ app.use(express.static(path.join(__dirname, '/client/public')));
 
 // add routes
 app.use(routes);
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 // app is listening on PORT
