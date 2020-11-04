b1c6b36d1ee6de4924922d6b696b5afd723e0f0b
MikChanna <channa.mik@gmail.com>
2020-11-04 18:41:24 -0500

    added isAuthenticated state

--- config/isAuthenticated.js
+++ config/isAuthenticated.js
@@ -1,10 +1,15 @@
 // This is middleware for restricting routes a user is not allowed to visit if not logged in
 module.exports = function(req, res, next) {
+
+  const [isAuthenticated, setisAuthenticated] = useState(false);
   // If the user is logged in, continue with the request to the restricted route
   if (req.user) {
-    return next();
+    setisAuthenticated(true)
+    console.log("loggedInUser", + isAuthenticated)
   }
 
   // If the user isn't logged in, redirect them to the login page
-  return res.redirect("/");
+  setisAuthenticated(false)
+  console.log("loggedInUser", + isAuthenticated)
+  return
 };
