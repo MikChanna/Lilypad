fa28e04eb97732dd1bdb174e72d3af61b4f350c8
MikChanna <channa.mik@gmail.com>
2020-11-03 21:59:57 -0500

    changed userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,40 +1,52 @@
-const passport = require("../config/passport");
+const passport = require("passport");
+const bcrypt = require("bcryptjs")
 const db = require("../models");
 
 // Defining methods for the userController
 module.exports = {
  
   createUser: function(req, res) {
-    console.log("reqbody", req.body)
+    console.log("reqbody:signup", req.body)
     db.User
-      .create({
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          phoneNumber: req.body.phoneNumber,
-          email: req.body.email,
-          password: req.body.password
-      })
-      .then(function() {
-          res.redirect(307, "/login");
-      })
-      .catch(function(err) {
-          console.log("err", err)
-          res.status(401).json(err)
+      .findOne({ username: req.body.username }, async (err, doc) => {
+        if (err) throw err;
+        if (doc) res.send("User Already Exists");
+        if (!doc) {
+          const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    
+          const newUser = new db.User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phoneNumber: req.body.phoneNumber,
+            username: req.body.username,
+            password: hashedPassword,
+
+          });
+          await newUser.save();
+          res.send("User Created");
+        }
       });
   },
-  login: function(req, res) {
-      passport.authenticate("local"), function(req, res) {
-        res.redirect(307, "/");
+  login: function(req, res, next) {
+    console.log("reqbody:login", req.body)
+    passport.authenticate("local", (err, user, info) => {
+      if (err) throw err;
+      if (!user) res.send("No User Exists");
+      else {
+        req.logIn(user, (err) => {
+          if (err) throw err;
+          res.send("Successfully Authenticated");
+          console.log(req.user);
+        });
       }
+    })(req, res, next);
   },
   findUserData: function(req, res) {
       if(!req.user) {
           res.json({})
       } else {
-          res.json({
-              email: req.user.email,
-              id: req.user.id
-          })
+        res.send(req.user); 
+          
         }
   },
   
