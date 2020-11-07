c61da3706446afcfc3e5c6c3dad997151ea896f6
Sarah Kerr <sarah.kerr93@gmail.com>
2020-11-07 00:17:08 -0500

    Reset files

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,57 +1,73 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./style.css";
-import API from "../../utils/UserAPI"
-import NavbarNoLinks from "../../components/NavbarNoLinks"
+import API from "../../utils/UserAPI";
+import NavbarNoLinks from "../../components/NavbarNoLinks";
 
-function Login () {
-
+function Login() {
   const [loginUser, setloginUser] = useState({
-    username:"",
-    password:"",
-  })
+    username: "",
+    password: "",
+  });
 
   const loginRequest = (event) => {
     event.preventDefault();
 
     API.loginUser({
-        username: loginUser.username,
-        password: loginUser.password,
-        withCredentials: true,
+      username: loginUser.username,
+      password: loginUser.password,
+      withCredentials: true,
     }).then((res) => {
-      console.log(res)
-      window.location.replace("/home")}
-    )
+      console.log("res", res);
+      if (res.data === "Wrong username or password") {
+        alert("Wrong username or password, try again");
+      } else {
+        window.location.replace("/home");
+      }
+    });
   };
-    
+
   function handleInputChange(event) {
     const { name, value } = event.target;
 
     setloginUser({ ...loginUser, [name]: value });
   }
 
-
-
-  return <div className = "ui container fluid">
-    <NavbarNoLinks/>
-    <div className = "credForm">
-      <form className = "ui form">
-          <div className = "field">
-              <label>Username</label>
-              <input  onChange={handleInputChange}type = "text" name="username" placeholder = "uesrname" id="username" />
+  return (
+    <div className="ui container fluid">
+      <NavbarNoLinks />
+      <div className="credForm">
+        <form className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input
+              onChange={handleInputChange}
+              type="text"
+              name="username"
+              placeholder="username"
+              id="username"
+            />
           </div>
-          <div className = "field">
-              <label>Password</label>
-              <input  onChange={handleInputChange}type = "password" name = "password" placeholder = "Password" id="password-input" />
+          <div className="field">
+            <label>Password</label>
+            <input
+              onChange={handleInputChange}
+              type="password"
+              name="password"
+              placeholder="Password"
+              id="password-input"
+            />
           </div>
-          <button className="ui button" onClick={loginRequest} type="submit">Log in</button>
-      </form>
-      <br/>
-      <p>Or sign up <a href="/signup">here</a></p>
-  </div>
-  </div>
-
+          <button className="ui button" onClick={loginRequest} type="submit">
+            Log in
+          </button>
+        </form>
+        <br />
+        <p>
+          Or sign up <a href="/signup">here</a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
-
-
--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./style.css";
 import API from "../../utils/UserAPI";
-import NavbarNoLinks from "../../components/NavbarNoLinks"
+import NavbarNoLinks from "../../components/NavbarNoLinks";
 
 function Signup() {
   const [userObject, setUserObject] = useState({
@@ -23,8 +23,13 @@ function Signup() {
       phoneNumber: userObject.phoneNumber,
       withCredentials: true,
     }).then((res) => {
-      console.log("user created");
-      window.location.replace("/");
+      console.log("res", res);
+      if (res.data === "User Already Exists") {
+        alert("User already exists");
+      } else {
+        window.location.replace("/");
+        res.send("Create account successful!");
+      }
     });
   };
 
@@ -35,69 +40,69 @@ function Signup() {
   }
 
   return (
-    <div className = "ui container fluid">
-      <NavbarNoLinks/>
-      <div className="credForm">
-      <form className="ui form ">
-        <div className="field">
-          <label>First Name</label>
-          <input
-            onChange={handleInputChange}
-            type="text"
-            name="firstName"
-            placeholder="First Name"
-            id="firstName"
-          />
-        </div>
-        <div className="field">
-          <label>Last Name</label>
-          <input
-            onChange={handleInputChange}
-            type="text"
-            name="lastName"
-            placeholder="Last Name"
-            id="lastName"
-          />
-        </div>
-        <div className="field">
-          <label>Phone Number</label>
-          <input
-            onChange={handleInputChange}
-            type="text"
-            name="phoneNumber"
-            placeholder="000-000-000"
-            id="phoneNumber"
-          />
-        </div>
-        <div className="field">
-          <label>Username</label>
-          <input
-            onChange={handleInputChange}
-            type="text"
-            name="username"
-            placeholder="username"
-            id="username"
-          />
-        </div>
-        <div className="field">
-          <label>Password</label>
-          <input
-            onChange={handleInputChange}
-            type="password"
-            name="password"
-            placeholder="Password"
-            id="password-input"
-          />
-        </div>
-        <button onClick={register} className="ui button signup" type="submit">
-          Sign Up
-        </button>
-      </form>
-      <br />
-      <p>
-        Or log in <a href="/login">here</a>
-      </p>
-    </div>
+    <div className="ui container fluid">
+      <NavbarNoLinks />
+      <div className="credFormS">
+        <form className="ui form ">
+          <div className="field">
+            <label>First Name</label>
+            <input
+              onChange={handleInputChange}
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              id="firstName"
+            />
+          </div>
+          <div className="field">
+            <label>Last Name</label>
+            <input
+              onChange={handleInputChange}
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              id="lastName"
+            />
+          </div>
+          <div className="field">
+            <label>Phone Number</label>
+            <input
+              onChange={handleInputChange}
+              type="text"
+              name="phoneNumber"
+              placeholder="000-000-000"
+              id="phoneNumber"
+            />
+          </div>
+          <div className="field">
+            <label>Username</label>
+            <input
+              onChange={handleInputChange}
+              type="text"
+              name="username"
+              placeholder="username"
+              id="username"
+            />
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <input
+              onChange={handleInputChange}
+              type="password"
+              name="password"
+              placeholder="Password"
+              id="password-input"
+            />
+          </div>
+          <button onClick={register} className="ui button signup" type="submit">
+            Sign Up
+          </button>
+        </form>
+        <br />
+        <p>
+          Or log in <a href="/login">here</a>
+        </p>
+      </div>
     </div>
   );
 }
