dfd577ee45db96f42fb1bb899dc847d11855fc8e
Sarah Kerr <sarah.kerr93@gmail.com>
2020-11-12 15:40:38 -0500

    adding remove button in gallery, calls remove function which currently just console logs the image id

--- client/src/components/ImageItem/index.js
+++ client/src/components/ImageItem/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ImageItem({ data }) {
+  const removeFriend = (id) => {
+    //add api call to remove the one photo from the mongodb
+    console.log(id);
+  };
+
   return (
-    // <div className="ui card green">
     <div className="image centered">
       <img src={data.url} alt="avatar" />
+      <span onClick={() => removeFriend(data._id)} className="remove">
+        Remove
+      </span>
     </div>
-    // </div>
   );
 }
 
